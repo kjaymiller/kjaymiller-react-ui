12a5451a1c941d238b63951f868ce04201319aa4
Jay Miller <kjaymiller@gmail.com>
2020-10-24 10:02:42 -0700

    initial commit

---
+++ build/precache-manifest.99db9b4cebf2bf4585d0b62a50cffe62.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54325c8a2846ada1410ccfe95836c95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2ff71c2b0898c91afb8",
+    "url": "/static/css/main.390516eb.css"
+  },
+  {
+    "revision": "a2ff71c2b0898c91afb8",
+    "url": "/static/js/main.8ec74475.js"
+  },
+  {
+    "revision": "95ea6e06508ae6cb347ab110e0d2cd19",
+    "url": "/static/js/main.8ec74475.js.LICENSE.txt"
+  }
+]);
